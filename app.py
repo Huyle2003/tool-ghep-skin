9c1cd0d982764a79af70427df574e48a1be228f4
Huyle2003 <ledanghuy2003.dlieya@gmail.com>
2026-03-06 17:39:16 +0700

    hoan thanh

--- app.py
+++ app.py
@@ -63,7 +63,6 @@ def upload_rate():
     file = request.files["file"]
 
     filename = str(uuid.uuid4()) + ".png"
-
     path = os.path.join(UPLOAD, filename)
 
     file.save(path)
@@ -73,19 +72,70 @@ def upload_rate():
     if img is None:
         return "Không đọc được ảnh"
 
-    left = 239
-    top = 155
-    width = 617
-    height = 1106
+    template_path = os.path.join(BASE_DIR, "profile.png")
+    template = cv2.imread(template_path)
 
-    crop = img[top:top+height, left:left+width]
+    if template is None:
+        return "Không tìm thấy profile.png"
+
+    # ======================
+    # ORB detect
+    # ======================
+
+    orb = cv2.ORB_create(2000)
+
+    kp1, des1 = orb.detectAndCompute(template, None)
+    kp2, des2 = orb.detectAndCompute(img, None)
+
+    if des1 is None or des2 is None:
+        return "Không detect được feature"
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    good = []
+
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+
+    if len(good) < 10:
+        return "Không tìm thấy profile"
+
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+    h, w = template.shape[:2]
+
+    pts = np.float32([[0,0],[0,h],[w,h],[w,0]]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts, M)
+
+    x1 = int(min(p[0][0] for p in dst))
+    y1 = int(min(p[0][1] for p in dst))
+    x2 = int(max(p[0][0] for p in dst))
+    y2 = int(max(p[0][1] for p in dst))
+
+    # ======================
+    # xác định vùng rate
+    # ======================
+
+    margin = 40
+
+    rate_x1 = max(0, x1 - margin)
+    rate_y1 = max(0, y1 - margin)
+    rate_x2 = min(img.shape[1], x2 + margin)
+    rate_y2 = min(img.shape[0], y2 + margin)
+
+    crop = img[rate_y1:rate_y2, rate_x1:rate_x2]
 
     save = os.path.join(RATE, "rate.png")
 
     cv2.imwrite(save, crop)
 
     return "ok"
-
 
 # =============================
 # Upload shop
@@ -183,6 +233,51 @@ def cut_skin():
 
     return jsonify({"skins": skins})
 
+def find_profile(bg):
+
+    template_path = os.path.join(BASE_DIR, "profile.png")
+    template = cv2.imread(template_path)
+
+    orb = cv2.ORB_create(2000)
+
+    kp1, des1 = orb.detectAndCompute(template, None)
+    kp2, des2 = orb.detectAndCompute(bg, None)
+
+    if des1 is None or des2 is None:
+        return None
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append(m)
+
+    if len(good) < 10:
+        return None
+
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+    if M is None:
+        return None
+
+    h,w = template.shape[:2]
+
+    pts = np.float32([[0,0],[0,h],[w,h],[w,0]]).reshape(-1,1,2)
+
+    dst = cv2.perspectiveTransform(pts,M)
+
+    x1 = int(min(p[0][0] for p in dst))
+    y1 = int(min(p[0][1] for p in dst))
+    x2 = int(max(p[0][0] for p in dst))
+    y2 = int(max(p[0][1] for p in dst))
+
+    return x1,y1,x2,y2
 
 # =============================
 # Ghép ảnh
@@ -191,12 +286,15 @@ def cut_skin():
 def merge():
 
     global bg_path
-
+    try:
+        paper_count = int(request.json.get("giay_ts", 0))
+    except:
+        paper_count = 0
     if bg_path is None:
         return jsonify({"error":"Chưa có background"})
 
     skins = request.json["skins"]
-
+    use_chest = request.json.get("ruong_cs", False)
     if len(skins) == 0:
         return jsonify({"error":"Chưa có skin"})
 
@@ -216,7 +314,28 @@ def merge():
 
     rate = cv2.copyMakeBorder(rate,5,5,5,5,cv2.BORDER_CONSTANT,value=[255,255,255])
 
-    bg[155:155+rate.shape[0],239:239+rate.shape[1]] = rate
+    pos = find_profile(bg)
+
+    if pos is None:
+        return jsonify({"error":"Không tìm thấy profile trong background"})
+
+    x1,y1,x2,y2 = pos
+
+    profile_w = x2 - x1
+    profile_h = y2 - y1
+
+    # resize rate đúng kích thước profile
+    rate = cv2.resize(rate,(profile_w,profile_h))
+
+    # đảm bảo không vượt biên
+    x1 = max(0,x1)
+    y1 = max(0,y1)
+    x2 = min(bg.shape[1],x1+profile_w)
+    y2 = min(bg.shape[0],y1+profile_h)
+
+    bg[y1:y2, x1:x2] = rate[0:(y2-y1),0:(x2-x1)]
+
+    
 
     left_margin = 890
     right_margin = 100
@@ -235,12 +354,66 @@ def merge():
 
     new_h = int(default_h * scale)
 
+    gap = 5
+    border = 5
     start_x = left_margin
 
-    for i, name in enumerate(skins):
+    # ===== load rương cs =====
+    chest = None
+
+    if use_chest:
+        chest_path = os.path.join(BASE_DIR, "ruong_cs.png")
+
+        if os.path.exists(chest_path):
+            chest = cv2.imread(chest_path)
+            chest = cv2.resize(chest, (350,300))
+            # thêm viền trắng
+            chest = cv2.copyMakeBorder(
+                chest,
+                5,5,5,5,
+                cv2.BORDER_CONSTANT,
+                value=(255,255,255)
+            )
+
+    skins_data = []
+    # ===== load gts =====
+    paper = None
+
+    if paper_count > 0:
+
+        paper_path = os.path.join(BASE_DIR,"giay_ts.png")
+
+        if os.path.exists(paper_path):
+
+            paper = cv2.imread(paper_path)
+
+            paper = cv2.resize(paper,(300,300))
+
+            # viền trắng
+            paper = cv2.copyMakeBorder(
+                paper,
+                5,5,5,5,
+                cv2.BORDER_CONSTANT,
+                value=(255,255,255)
+            )
+    if paper is not None:
+
+        text = str(paper_count)
+
+        cv2.putText(
+            paper,
+            text,
+            (180,220),   # tọa độ chữ
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.2,
+            (255,255,255),
+            3,
+            cv2.LINE_AA
+        )
+    # ===== load skin =====
+    for name in skins:
 
         path = os.path.join(SKIN, name)
-
         skin = cv2.imread(path)
 
         if skin is None:
@@ -248,15 +421,86 @@ def merge():
 
         skin = cv2.resize(skin,(new_w,new_h))
 
-        skin = cv2.copyMakeBorder(skin,5,5,5,5,cv2.BORDER_CONSTANT,value=[255,255,255])
+        skin = cv2.copyMakeBorder(
+            skin,
+            border, border, border, border,
+            cv2.BORDER_CONSTANT,
+            value=(255,255,255)
+        )
+
+        skins_data.append(skin)
+
+    # ===== ghép skin vào background =====
+    for i, skin in enumerate(skins_data):
 
         h, w = skin.shape[:2]
 
-        x = start_x + i * w
-
+        x = start_x + i * (w - border*2 + gap)
         y = bg.shape[0] - h - bottom_margin
 
         bg[y:y+h, x:x+w] = skin
+
+
+    # ===== ghép rương chung sức =====
+    if chest is not None and len(skins_data) > 0:
+
+        first_skin = skins_data[0]
+
+        h, w = first_skin.shape[:2]
+
+        first_x = start_x
+        first_y = bg.shape[0] - h - bottom_margin
+
+        chest_x = first_x
+        chest_y = first_y - chest.shape[0] - 15
+
+        bg[chest_y:chest_y+chest.shape[0], chest_x:chest_x+chest.shape[1]] = chest
+
+        # viền trắng 5px full
+        skin = cv2.copyMakeBorder(
+            skin,
+            border, border, border, border,
+            cv2.BORDER_CONSTANT,
+            value=(255,255,255)
+        )
+
+        h, w = skin.shape[:2]
+
+        # khoảng cách giữa các skin chỉ 5px
+        x = start_x + i * (w - border*2 + gap)
+        y = bg.shape[0] - h - bottom_margin
+
+        bg[y:y+h, x:x+w] = skin
+
+    # ===== ghép giấy tuyệt sắc =====
+
+    if paper is not None and len(skins_data) > 0:
+
+        first_skin = skins_data[0]
+
+        h, w = first_skin.shape[:2]
+
+        first_x = start_x
+        first_y = bg.shape[0] - h - bottom_margin
+
+        if chest is not None:
+
+            chest_x = first_x
+            chest_y = first_y - chest.shape[0] - 15
+
+            paper_x = chest_x + chest.shape[1] + 10
+            paper_y = chest_y
+
+        else:
+
+            paper_x = first_x
+            paper_y = first_y - paper.shape[0] - 15
+
+
+        bg[
+            paper_y:paper_y+paper.shape[0],
+            paper_x:paper_x+paper.shape[1]
+        ] = paper
 
     save = os.path.join(RESULT,"final.png")
 
